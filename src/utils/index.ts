c6e6fd8ac8eb8ee7d39d57f488d7e95debcc1774
Лысов Глеб <lysovg9@gmail.com>
2022-05-18 15:08:40 +0300

    Added: utils (localstorage)

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+export const test = {};
+
+export const connectLocalStorage = (key: string) => {
+  const handle = (state: any) => {
+    localStorage.setItem(key, window.btoa(JSON.stringify(state)));
+  };
+
+  handle.init = <T>(state: T) => {
+    const value = localStorage.getItem(key);
+    if (value) return JSON.parse(window.atob(value)) as T;
+    localStorage.setItem(key, window.btoa(JSON.stringify(state)));
+    return state;
+  };
+
+  return handle;
+};

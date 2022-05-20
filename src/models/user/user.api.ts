7afa7d87ec6857bed3c5dd609e8a87d53aa70b2a
Лысов Глеб <lysovg9@gmail.com>
2022-05-20 10:30:12 +0300

    Added: user module

---
+++ src/models/user/user.api.ts
@@ -0,0 +1,4 @@
+export default {
+  // getProfile: (username: any) => kyInstancce.get(`user/get/${username}`), // eslint-disable-line @typescript-eslint/no-explicit-any
+  // test: () => kyInstancce.get('app/test/get'),
+};
